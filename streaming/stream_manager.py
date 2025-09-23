e5d63112d3abeb49d3a77f4034a2b97914dca78c
lukasmeggle <lukas3224@gmail.com>
2025-09-23 12:12:45 +0200

    Streaming: Create dir for recording when rec is enabled in config

--- streaming/stream_manager.py
+++ streaming/stream_manager.py
@@ -3,6 +3,8 @@ from .camera_pipeline import IRCameraPipeline, PiCameraPipeline
 
 class StreamManager:
     def __init__(self, config_file):
+
+        # Load configuration
         with open(config_file) as f:
             self.cfg = yaml.safe_load(f)
         self.log_dir = os.path.join(self.cfg["log_dir"], datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
@@ -10,15 +12,20 @@ class StreamManager:
         self.laptop_ip = self.cfg["laptop_ip"]
         self.pipelines = []
 
+        # Create record directory if recording is enabled
+        if self.cfg["ir_camera"]["record"] or self.cfg["pi_camera"]["record"]:
+            os.makedirs(self.record_dir, exist_ok=True)
+
+        # Initialize pipelines based on config
         if self.cfg["ir_camera"]["stream"] or self.cfg["ir_camera"]["record"] or self.cfg["ir_camera"]["display"]:
             self.pipelines.append(IRCameraPipeline(self.cfg["ir_camera"], self.laptop_ip, self.log_dir, self.record_dir))
-
         if self.cfg["pi_camera"]["stream"] or self.cfg["pi_camera"]["record"] or self.cfg["pi_camera"]["display"]:
             self.pipelines.append(PiCameraPipeline(self.cfg["pi_camera"], self.laptop_ip, self.log_dir, self.record_dir))
 
-        # ComposePipeline später ergänzen
+        # ComposePipeline TBD
 
     def start_all(self):
+        ''' Start all configured pipelines'''
         for p in self.pipelines:
             if p.cmd:  # skip wenn keine Sinks
                 p.start()
